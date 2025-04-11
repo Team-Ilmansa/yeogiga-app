18af0de4283566ccf37758a2b180be7ceb79a719
NoGravyBeef <dyfldrpf9@naver.com>
2025-04-11 20:42:00 +0900

    📌Chore: Codemagic, Google play 내부 테스트 연동 (CD) 및 자동화

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,21 +1,43 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+// 🔐 keystore 설정 로딩
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(keystorePropertiesFile.inputStream())
 }
 
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
 android {
-    compileSdk = 33
     namespace = "com.ilmansa.yeogiga.yeogiga"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
 
     defaultConfig {
         applicationId = "com.ilmansa.yeogiga.yeogiga"
-        minSdk = 23
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
+    // ✅ 릴리즈 빌드 서명 설정
     signingConfigs {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
@@ -32,13 +54,8 @@ android {
             isShrinkResources = false
         }
     }
+}
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+flutter {
+    source = "../.."
 }
