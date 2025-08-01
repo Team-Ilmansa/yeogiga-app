981f2b39b972a15faaf032c9d034670c6c1b76c4
NoGravyBeef <dyfldrpf9@naver.com>
2025-08-01 22:39:43 +0900

    feat: API URL과 카카오 리다이렉트 URI를 환경변수로 이동
    - API_BASE_URL을 .env로 이동하여 환경별 관리 가능
    - KAKAO_REDIRECT_URI를 .env로 이동하여 설정 관리 개선
    - data.dart에서 dotenv로 읽도록 수정

--- lib/common/const/data.dart
+++ lib/common/const/data.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // secure storage용 key
 const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
@@ -9,9 +10,10 @@ final storage = FlutterSecureStorage();
 final emulatorIp = '192.168.0.18:3000';
 final simulatorIp = '127.0.0.1:3000';
 
-// final ip = Platform.isIOS ? simulatorIp : emulatorIp;
-final ip = 'api.yeogiga.com';
+// 환경변수에서 API URL 읽기
+final ip = dotenv.get('API_BASE_URL');
 
-final kakaoRedirectUri = 'http://yeogiga.com/oauth/kakao/callback';
+// 환경변수에서 카카오 리다이렉트 URI 읽기  
+final kakaoRedirectUri = dotenv.get('KAKAO_REDIRECT_URI');
 // final kakaoRedirectUri =
 //     'kakao0e2b4c6886ca5a40118dd41935b17592://login-callback';
